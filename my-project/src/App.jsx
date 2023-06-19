c34561d8c9b7c1ef0672f30a06108a986c4d4664
PedroHGaspar <ordep12@hotmail.com.br>
2023-06-19 12:27:41 -0300

    Update App.js.

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,23 +1,28 @@
 import { BrowserRouter } from "react-router-dom";
-
-import { About, Contact, Experience, Navbar, Works, } from "./components";
+import { About, Contact, Experience, Navbar, Works } from "./components";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+      <div
+        style={{
+          backgroundColor: "#292524",
+          minHeight: "100vh",
+          minWidth: "100vw",
+        }}
+      >
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
         </div>
         <About />
         <Experience />
         <Works />
-        <div className='relative z-0'>
+        <div className="relative z-0">
           <Contact />
         </div>
       </div>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
--- my-project/src/components/index.js
+++ my-project/src/components/index.js
@@ -9,5 +9,5 @@ export {
   About,
   Experience,
   Works,
-  Contact
+  Contact,
 }
