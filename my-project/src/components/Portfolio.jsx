69b0444042e6e6259f135e0cd284d5e59073ab34
PedroHGaspar <ordep12@hotmail.com.br>
2023-06-26 11:59:51 -0300

    corpo do projectPortfolio feito com base no experience.

--- my-project/src/components/Portfolio.jsx
+++ my-project/src/components/Portfolio.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { PortfolioCard, portfolio } from "./PortfolioProjects.tsx";
+import { MotionDiv } from "./use.client";
 
-const Portfolio = () => {
+const PortfolioProjects = () => {
   return (
-    <div>Portfolio</div>
+    <section id="portfolio" style={{ maxWidth: "1320px", margin: "0 auto"}}>
+    <MotionDiv
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="relative mx-auto flex min-h-screen max-w-full flex-col items-center gap-12 overflow-hidden py-24 px-4 text-left md:px-10"
+    >
+      <h3 className="font-cal text-3xl uppercase tracking-[15px] text-gray-400 md:tracking-[20px] font-bold">
+        Portfolio and Projects
+      </h3>
+      <div className="flex w-full snap-x snap-mandatory space-x-5 overflow-x-scroll md:p-10 scrollbar-thin scrollbar-thumb-amber-500 scrollbar-track-gray-900">
+        {portfolio.map((portfolio) => (
+          <PortfolioCard key={portfolio.title} {...portfolio} />
+        ))}
+      </div>
+    </MotionDiv>
+  </section>
   )
 }
 
-export default Portfolio
+export default PortfolioProjects
